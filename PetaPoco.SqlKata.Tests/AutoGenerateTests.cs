e4397f6b875f59c93d41f67fe3f103cb169e8815
Aaron Sherber <aaron@sherber.com>
2018-10-18 16:17:51 -0400

    Refactor tests

--- PetaPoco.SqlKata.Tests/AutoGenerateTests.cs
+++ PetaPoco.SqlKata.Tests/AutoGenerateTests.cs
@@ -15,67 +15,48 @@ namespace PetaPoco.SqlKata.Tests
         public void ForType_Simple_Class()
         {
             var q = new Query().ForType<MyClass>();
-            var expected = new FromClause() { Table = "MyClass", Alias = "MyClass", Component = "from" };
-            q.Clauses.Should().BeEquivalentTo(expected);
+            var expected = new Query("MyClass");
+            q.Should().BeEquivalentTo(expected);
         }
 
         [Fact]
         public void ForType_With_TableName()
         {
             var q = new Query().ForType<MyClassWithName>();
-            var expected = new FromClause() { Table = "TableName", Alias = "TableName", Component = "from" };
-            q.Clauses.Should().BeEquivalentTo(expected);
+            var expected = new Query("TableName");
+            q.Should().BeEquivalentTo(expected);
         }
 
         [Fact]
         public void Generate_SimpleClass()
         {
             var q = new Query().GenerateSelect<MyClass>();
-            var expected = new AbstractClause[]
-            {
-                new FromClause() { Table = "MyClass", Alias = "MyClass", Component = "from" },
-                new Column() { Component = "select", Name = "ID" },
-                new Column() { Component = "select", Name = "Name" },
-            };
-            q.Clauses.Should().BeEquivalentTo(expected);
+            var expected = new Query("MyClass").Select("ID", "Name");
+            q.Should().BeEquivalentTo(expected);
         }
 
         [Fact]
         public void Generate_NoFields()
         {
             var q = new Query().GenerateSelect<NoFields>();
-            var expected = new AbstractClause[]
-            {
-                new FromClause() { Table = "NoFields", Alias = "NoFields", Component = "from" },
-                new RawColumn() { Component = "select", Expression = "NULL", Bindings = new object[0] },  
-            };
-            q.Clauses.Should().BeEquivalentTo(expected);
+            var expected = new Query("NoFields").SelectRaw("NULL");
+            q.Should().BeEquivalentTo(expected);
         }
 
         [Fact]
         public void Generate_With_TableName()
         {
             var q = new Query().GenerateSelect<MyClassWithName>();
-            var expected = new AbstractClause[]
-            {
-                new FromClause() { Table = "TableName", Alias = "TableName", Component = "from" },
-                new Column() { Component = "select", Name = "ID" },
-                new Column() { Component = "select", Name = "Name" },
-            };
-            q.Clauses.Should().BeEquivalentTo(expected);
+            var expected = new Query("TableName").Select("ID", "Name");
+            q.Should().BeEquivalentTo(expected);
         }
 
         [Fact]
         public void Generate_With_ColumnNames()
         {
             var q = new Query().GenerateSelect<MyClassWithColumnNames>();
-            var expected = new AbstractClause[]
-            {
-                new FromClause() { Table = "MyClassWithColumnNames", Alias = "MyClassWithColumnNames", Component = "from" },
-                new Column() { Component = "select", Name = "ID_FIELD" },
-                new Column() { Component = "select", Name = "NAME_FIELD" },
-            };
-            q.Clauses.Should().BeEquivalentTo(expected);
+            var expected = new Query("MyClassWithColumnNames").Select("ID_FIELD", "NAME_FIELD");
+            q.Should().BeEquivalentTo(expected);
         }
     }
 
